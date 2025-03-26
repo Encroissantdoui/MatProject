6219d9a012f75a17bd0af09e63ea3b2bfae108c3
mkiahsjsb <example@example.com>
2025-03-26 09:33:39 +0100

    Imported matrix multiplication

--- calc.cpp
+++ calc.cpp
@@ -98,9 +98,7 @@ float determinant(float matrix[3][3])
     float det = maind - secd;
     
     return det;
-}
-
-void adjg(float matrix[3][3], float adj[3][3])
+} void adjg(float matrix[3][3], float adj[3][3])
 {
     adj[0][0] =  matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1];
     adj[1][0] = -(matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0]);
@@ -115,9 +113,7 @@ void adjg(float matrix[3][3], float adj[3][3])
     adj[2][2] =  matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
 
     
-}
-
-void inverse(float adj[3][3], float det, float inv[3][3])
+} void inverse(float adj[3][3], float det, float inv[3][3])
 {
     if (det == 0) {
         std::cout << "No existe inversa \n";
@@ -128,6 +124,47 @@ void inverse(float adj[3][3], float det, float inv[3][3])
             inv[i][j] = adj[i][j] / det;
         }
     }
+}
+int main() {
+    int A[3][3], B[3][3], C[3][3] = {0};
+
+
+    cout << "Introdueix els valors de la matriu A (3x3):" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "A[" << i << "][" << j << "] = ";
+            cin >> A[i][j];
+        }
+    }
+
+    // Entrada de valors per a la matriu B
+    cout << "Introdueix els valors de la matriu B (3x3):" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "B[" << i << "][" << j << "] = ";
+            cin >> B[i][j];
+        }
+    }
+
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < 3; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    
+    cout << "La matriu resultat C és:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "matriuC[" << i << "][" << j << "] = " << C[i][j] << endl;
+        }
+    }
+
+    return 0;
 }
 
 
